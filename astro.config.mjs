70b1efa5c24b36e2edb1401cfd261a3f7a0bdf6d
Nguyen Quang Vinh <contact.nguyenquangvinh@gmail.com>
2026-07-19 14:14:57 +0700

    config: set site and base for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://aac79.com.vn",
+  base: "/",
   redirects: {
     "/ho-so-nang-luc": "/ho-so-nang-luc/aac79-protech/",
   },
